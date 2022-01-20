4ec554df168b6d7b9901153dd1039b74838b76eb
D'Kerjain <50320881+shindipurnama@users.noreply.github.com>
2022-01-20 21:00:02 +0700

    Kurang js kota, scan, geoloc
    sisanya sudah innsyallah

--- resources/views/user/customer/applyInvestasi.blade.php
+++ resources/views/user/customer/applyInvestasi.blade.php
@@ -1,31 +1,30 @@
 @extends('layout.user.index')
 @section('title', 'Register Petani Mitra')
 @section('content')
-<div class="content-wrapper">
 
-    <!-- Main content -->
-    <section class="content m-4">
-
-      <!-- Default box -->
-      <div class="card card-solid">
-        <div class="card-body">
-          <div class="row">
+<section class="kahfibwa mt-5">
+    <div class="container">
+    <div class="row">                
+        <div class="col-md-12">
+            <div class="card">
+                    <div class="card-body card-block">
+                      <div class="row">
             <div class="col-12 col-sm-6">
-              <h3 class="d-inline-block d-sm-none">Jawara Farm Surabaya</h3>
+              <h3 for="exampleFormControlInput1" class="d-inline-block d-sm-none">Jawara Farm Surabaya</h3>
               <div class="col-12">
                 <img width="100%" src="vendor/images/7185.jpg" class="product-image" alt="Product Image">
                 
               </div>
             </div>
-            <div class="col-12 col-sm-6">
-              <h3 class="my-3">Jawara Farm Surabaya</h3>
+            <div for="exampleFormControlInput1" class="col-12 col-sm-6">
+              <h3 for="exampleFormControlInput1" class="my-3">Jawara Farm Surabaya</h3>
               <b>Alamat :</b><p>Jl. Ketintang No 145</p>
               <b>No Telp :</b><p>0812483819210</p>
               <b>Periode</b><p>6 Bulan</p>
               <hr>
               
               
-              <div class="bg-gray py-2 px-3 mt-4">
+              <div for="exampleFormControlInput1" class="bg-gray py-2 px-3 mt-4">
                 <h4 class="mt-0">
                   <small>Jumlah Kebutuhan</small>
                 </h4>
@@ -33,17 +32,17 @@
                   Rp. 3.000.000
                 </h2>
               </div>
-              <div class="row">
+              <div for="exampleFormControlInput1" class="row">
                 <div class="bg-gray py-2 px-3 ml-3">
                   <h4 class="mt-0">
                     <small>Masukan Jumlah Beli</small>
                   </h4>
                   <h4 class="mb-0">
-                  <input class="ml-4">
+                  <input class="form-control ml-4">
                   </h4>
                 </div>
 
-                <div class="m-4">
+                <div for="exampleFormControlInput1" class="m-4">
                   <a href="/pembayaran">
                   <div class="btn btn-success btn-lg btn-flat">
                     <i class="fas fa-cart-plus fa-lg mr-2"></i>
@@ -66,14 +65,14 @@
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae condimentum erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed posuere, purus at efficitur hendrerit, augue elit lacinia arcu, a eleifend sem elit et nunc. Sed rutrum vestibulum est, sit amet cursus dolor fermentum vel. Suspendisse mi nibh, congue et ante et, commodo mattis lacus. Duis varius finibus purus sed venenatis. Vivamus varius metus quam, id dapibus velit mattis eu. Praesent et semper risus. Vestibulum erat erat, condimentum at elit at, bibendum placerat orci. Nullam gravida velit mauris, in pellentesque urna pellentesque viverra. Nullam non pellentesque justo, et ultricies neque. Praesent vel metus rutrum, tempus erat a, rutrum ante. Quisque interdum efficitur nunc vitae consectetur. Suspendisse venenatis, tortor non convallis interdum, urna mi molestie eros, vel tempor justo lacus ac justo. Fusce id enim a erat fringilla sollicitudin ultrices vel metus. 
               </div>
               <div class="tab-pane fade" id="product-comments" role="tabpanel" aria-labelledby="product-comments-tab"> 
-								<b>Simulasi Keuntungan :</b>
-								<div class="col-12 col-sm-12">
+								<b for="exampleFormControlInput1">Simulasi Keuntungan :</b>
+								<div for="exampleFormControlInput1" class="col-12 col-sm-12">
 									<div class="row m-1 mt-4">
 										<div class="col-6">
 											<p>Persentase Keuntungan :</p>
 										</div>
 										<div class="col-6">
-											<b class="ml-4">10%</b>
+											<b class="ml-4">3%</b>
 										</div>
 									</div>	
 									<div class="row m-1">
@@ -81,19 +80,22 @@
 											<p>Masukan Jumlah Investasi :</p>
 										</div>
 										<div class="col-4">
-											<input class="ml-4">
-											<p  class="ml-4">*minimal Rp.100.000</p>
+											<input type="number" class="form-control" id="hitung" class="ml-4">
+                      <p class="ml-4">*Min Rp. 100.000</p>
 										</div>
 										<div class="col-2">
-											<button type="submit" class="btn btn-success btn-block ml-4">Hitung</button>
+											<button type="submit" onclick="recount()" class="btn btn-success btn-lg btn-flat ml-4">Hitung</button>
 										</div>
 									</div>
 									<div class="row m-1">
 										<div class="col-6">
 											<p>Hasil Investasi :</p>
 										</div>
-										<div class="col-6">
-											<b class="ml-4">Rp. 0</b>
+										<div class="col-2">
+											<b class="ml-4">Rp. </b>
+										</div>
+										<div class="col-4">
+											<b  id="hasil" > </b>
 										</div>
 									</div>
 								</div>								
@@ -103,12 +105,22 @@
 							</div>
             </div>
           </div>
+                    </div>
+                
+            </div>
         </div>
-        <!-- /.card-body -->
-      </div>
-      <!-- /.card -->
+    </div>
+</div>
+</section>
 
-    </section>
-    <!-- /.content -->
-  </div>
+
+
+  <script>
+    function recount(){
+      var sembarang = document.getElementById("hitung").value;
+      console.log(sembarang);
+      var total = parseInt(103/100*sembarang);
+      document.getElementById("hasil").innerHTML = total;
+    }
+  </script>
 @endsection
--- resources/views/user/petani/applyProposal.blade.php
+++ resources/views/user/petani/applyProposal.blade.php
@@ -8,7 +8,7 @@
             <div class="col-md-12">
                 <div class="card mb-3">
 
-                    <div class="row">
+                    <div class="row justify-content-center">
 
                         
                     <div class="col-8">
@@ -44,8 +44,12 @@
                         </div>
 
                         <div class="col-4">
-                        <img class="p-2 pt-2 mb-4" width="100%" height="360" src="" id="img" name="img">
-                        <center><input type="file" class="btn btn-success" value="Upload Profile Proyek"></center>
+                        <label class="mt-4">
+                            <div id="output"  style="width: 327px; height:362px; border: solid 1px;"></div>
+                            <input class="form-control" type="text" id="inputFoto2" hidden>
+                            <center><input type="file" class="input-file btn btn-success mt-2" id="inputFoto" accept="image/png, image/jpg, image/jpeg" value="Upload Profile Proyek"></center>
+                        </label>                           
+
                         </div>
 
                     </div>
@@ -53,149 +57,165 @@
                     
                 </div>
 
-                
+                                
                 <section class="mt-5" id="proyek">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-sm-9 text-center proyek border-bottom">
-                <h1>Proyek Pembiayaan Anda</h1>                    
-            </div>
-        </div>
-        <div  class="row my-5 px-4" >
-            <div class="row">
-                <!-- Item -->
-                <div class="col-md-4 mb-5">
-                    <div class="bg-white rounded shadow">
-                        <a href="#" data-toggle="lightbox">
-                            <img src="vendor/images/7185.jpg" alt="" class="img-fluid card-img-top galeri px-2 py-2">                    
-                        </a>
-                        <div class="p-4">
-                            <!-- Jika Ingin Menambah Judul dan Caption Gambar -->
-                            <h5 class="cl-hijau">Pemasaran hasil panen</h5>
-                            <p class="small text-muted mb-2">Dana yang Dibutuhkan :</p>
-                            <h5 class="text-black border-bottom pb-2">Rp. 300.000</h5>
-                            <div class="row">
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Tingkat Resiko</p>
-                                    <p class="cl-hijau">Rendah</p>
-                                </div>
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Akad</p>
-                                    <p class="cl-hijau">Mudharabah</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Estimasi Profit</p>
-                                    <p class="cl-hijau">8% - 20%</p>
-                                </div>
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Periode Kontrak</p>
-                                    <p class="cl-hijau">4 Minggu</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <h5 class="text-black" style="font-size: 16px !important">Dana Terkumpul :</h5>
-                                </div>
-                                <div class="col-6">
-                                    <p class="cl-hijau text-right mb-0">0/100%</p>
-                                </div>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-sm-9 text-center proyek border-bottom">
+                                <h1>Proyek Pembiayaan Anda</h1>                    
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-5">
-                    <div class="bg-white rounded shadow">
-                        <a href="#" data-toggle="lightbox">
-                            <img src="vendor/images/7185.jpg" alt="" class="img-fluid card-img-top galeri px-2 py-2">                    
-                        </a>
-                        <div class="p-4">
-                            <!-- Jika Ingin Menambah Judul dan Caption Gambar -->
-                            <h5 class="cl-hijau">Pemasaran hasil panen</h5>
-                            <p class="small text-muted mb-2">Dana yang Dibutuhkan :</p>
-                            <h5 class="text-black border-bottom pb-2">Rp. 300.000</h5>
+                        <div  class="row my-5 px-4" >
                             <div class="row">
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Tingkat Resiko</p>
-                                    <p class="cl-hijau">Rendah</p>
+                                <!-- Item -->
+                                <div class="col-md-4 mb-5">
+                                    <div class="bg-white rounded shadow">
+                                        <a href="#" data-toggle="lightbox">
+                                            <img src="vendor/images/7185.jpg" alt="" class="img-fluid card-img-top galeri px-2 py-2">                    
+                                        </a>
+                                        <div class="p-4">
+                                            <!-- Jika Ingin Menambah Judul dan Caption Gambar -->
+                                            <h5 class="cl-hijau">Pemasaran hasil panen</h5>
+                                            <p class="small text-muted mb-2">Dana yang Dibutuhkan :</p>
+                                            <h5 class="text-black border-bottom pb-2">Rp. 300.000</h5>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Tingkat Resiko</p>
+                                                    <p class="cl-hijau">Rendah</p>
+                                                </div>
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Akad</p>
+                                                    <p class="cl-hijau">Mudharabah</p>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Estimasi Profit</p>
+                                                    <p class="cl-hijau">8% - 20%</p>
+                                                </div>
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Periode Kontrak</p>
+                                                    <p class="cl-hijau">4 Minggu</p>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <h5 class="text-black" style="font-size: 16px !important">Dana Terkumpul :</h5>
+                                                </div>
+                                                <div class="col-6">
+                                                    <p class="cl-hijau text-right mb-0">0/100%</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Akad</p>
-                                    <p class="cl-hijau">Mudharabah</p>
+                                <div class="col-md-4 mb-5">
+                                    <div class="bg-white rounded shadow">
+                                        <a href="#" data-toggle="lightbox">
+                                            <img src="vendor/images/7185.jpg" alt="" class="img-fluid card-img-top galeri px-2 py-2">                    
+                                        </a>
+                                        <div class="p-4">
+                                            <!-- Jika Ingin Menambah Judul dan Caption Gambar -->
+                                            <h5 class="cl-hijau">Pemasaran hasil panen</h5>
+                                            <p class="small text-muted mb-2">Dana yang Dibutuhkan :</p>
+                                            <h5 class="text-black border-bottom pb-2">Rp. 300.000</h5>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Tingkat Resiko</p>
+                                                    <p class="cl-hijau">Rendah</p>
+                                                </div>
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Akad</p>
+                                                    <p class="cl-hijau">Mudharabah</p>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Estimasi Profit</p>
+                                                    <p class="cl-hijau">8% - 20%</p>
+                                                </div>
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Periode Kontrak</p>
+                                                    <p class="cl-hijau">4 Minggu</p>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <h5 class="text-black" style="font-size: 16px !important">Dana Terkumpul :</h5>
+                                                </div>
+                                                <div class="col-6">
+                                                    <p class="cl-hijau text-right mb-0">0/100%</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Estimasi Profit</p>
-                                    <p class="cl-hijau">8% - 20%</p>
+                                <div class="col-md-4 mb-5">
+                                    <div class="bg-white rounded shadow">
+                                        <a href="#" data-toggle="lightbox">
+                                            <img src="vendor/images/7185.jpg" alt="" class="img-fluid card-img-top galeri px-2 py-2">                    
+                                        </a>
+                                        <div class="p-4">
+                                            <!-- Jika Ingin Menambah Judul dan Caption Gambar -->
+                                            <h5 class="cl-hijau">Pemasaran hasil panen</h5>
+                                            <p class="small text-muted mb-2">Dana yang Dibutuhkan :</p>
+                                            <h5 class="text-black border-bottom pb-2">Rp. 300.000</h5>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Tingkat Resiko</p>
+                                                    <p class="cl-hijau">Rendah</p>
+                                                </div>
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Akad</p>
+                                                    <p class="cl-hijau">Mudharabah</p>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Estimasi Profit</p>
+                                                    <p class="cl-hijau">8% - 20%</p>
+                                                </div>
+                                                <div class="col-6">
+                                                    <p class="text-black mb-0">Periode Kontrak</p>
+                                                    <p class="cl-hijau">4 Minggu</p>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <h5 class="text-black" style="font-size: 16px !important">Dana Terkumpul :</h5>
+                                                </div>
+                                                <div class="col-6">
+                                                    <p class="cl-hijau text-right mb-0">0/100%</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Periode Kontrak</p>
-                                    <p class="cl-hijau">4 Minggu</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <h5 class="text-black" style="font-size: 16px !important">Dana Terkumpul :</h5>
-                                </div>
-                                <div class="col-6">
-                                    <p class="cl-hijau text-right mb-0">0/100%</p>
-                                </div>
-                            </div>
+                                <!-- End -->
+                            </div>    
                         </div>
                     </div>
-                </div>
-                <div class="col-md-4 mb-5">
-                    <div class="bg-white rounded shadow">
-                        <a href="#" data-toggle="lightbox">
-                            <img src="vendor/images/7185.jpg" alt="" class="img-fluid card-img-top galeri px-2 py-2">                    
-                        </a>
-                        <div class="p-4">
-                            <!-- Jika Ingin Menambah Judul dan Caption Gambar -->
-                            <h5 class="cl-hijau">Pemasaran hasil panen</h5>
-                            <p class="small text-muted mb-2">Dana yang Dibutuhkan :</p>
-                            <h5 class="text-black border-bottom pb-2">Rp. 300.000</h5>
-                            <div class="row">
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Tingkat Resiko</p>
-                                    <p class="cl-hijau">Rendah</p>
-                                </div>
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Akad</p>
-                                    <p class="cl-hijau">Mudharabah</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Estimasi Profit</p>
-                                    <p class="cl-hijau">8% - 20%</p>
-                                </div>
-                                <div class="col-6">
-                                    <p class="text-black mb-0">Periode Kontrak</p>
-                                    <p class="cl-hijau">4 Minggu</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <h5 class="text-black" style="font-size: 16px !important">Dana Terkumpul :</h5>
-                                </div>
-                                <div class="col-6">
-                                    <p class="cl-hijau text-right mb-0">0/100%</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End -->
-            </div>    
-        </div>
-    </div>
-</section>
+                </section>
             </div>
             <div class="col-2"></div>
         </div>
     </div>
 </section>
 
+
+
+<script>
+    inputFoto.onchange = evt => {
+        const [file] = inputFoto.files
+        if (file) {
+            src = URL.createObjectURL(file)
+
+            document.getElementById('output').innerHTML = 
+            '<img src="'+src+'" id="hasil" style="width: 325px; height:360px;"/>';
+            
+            var hasil = $('#hasil').attr('src');
+            $('#inputFoto2').val(hasil);
+        }
+    }
+</script>
 @endsection
